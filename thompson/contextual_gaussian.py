9d60d2618b68390886c89e6f3159b904c43ec5bc
TakumaTanaka <tak.1025.rmk@gmail.com>
2023-03-06 17:32:12 +0900

    fix: some bug

--- thompson/contextual_gaussian.py
+++ thompson/contextual_gaussian.py
@@ -2,16 +2,6 @@ import numpy as np
 
 MU = np.array([3.0, 2.0, 1.0])
 SIGMA = 10.0
-SIGMA_0 = 1.0
-
-
-def update_parameters(x, mu, gamma, alpha, beta):
-    alpha += 0.5
-    beta += 0.5 * (x * x + gamma * mu * mu)
-    gamma += 1.0
-    mu = ((gamma - 1.0) * mu + x) / gamma
-    beta -= 0.5 * gamma * mu * mu
-    return mu, gamma, alpha, beta
 
 
 def main(n_sample: int):
@@ -19,12 +9,13 @@ def main(n_sample: int):
     n_item = len(MU)
     n_user = 100
     n_attribute = 5
+    sigma_0 = 1.0
     theta_attribute = np.random.randn(n_attribute)
     theta_true = np.concatenate([MU, theta_attribute])
     user_attribute = np.random.randn(n_user, n_attribute)
 
     # initialization
-    a_inv = SIGMA_0 / SIGMA * np.eye(len(theta_true))
+    a_inv = sigma_0 / SIGMA * np.eye(len(theta_true))
     b = np.zeros(len(theta_true))
 
     # for record
@@ -35,7 +26,7 @@ def main(n_sample: int):
         mu = a_inv @ b
         gamma = SIGMA * a_inv
         gamma_sqrt = np.linalg.cholesky(gamma)
-        theta_sample = mu + np.random.randn(len(mu)) @ gamma_sqrt
+        theta_sample = mu + gamma_sqrt @ np.random.randn(len(mu))
         action = np.hstack([np.eye(n_item), attribute[np.newaxis, :].repeat(n_item, axis=0)])
         value = action @ theta_sample
         arm = np.argmax(value)
@@ -50,13 +41,15 @@ def main(n_sample: int):
         result[arm].append(reward)
     print('count selection of arms')
     for item in range(n_item):
-        print('  item', item, '|| N(', MU[item], ',', SIGMA ** 2, '):')
+        print('  item', item, '|| N(', MU[item], ',', SIGMA, '):')
         print('    count :', len(result[item]))
         if result[item]:
             print('    mean  :', np.mean(result[item]))
             print('    stdev :', np.std(result[item]))
     regret = n_sample * MU.max() - sum(sum(r) for r in result)
     print('regret:', regret)
+    print('true param:', theta_true)
+    print('pred param:', a_inv @ b)
 
 
 if __name__ == "__main__":
